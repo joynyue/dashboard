b0f5d1844bbc927f7440887938fdde4031497ee3
Joy Zhang <joynyue@gmail.com>
2020-09-27 17:14:56 +1000

    Updates

---
+++ precache-manifest.7b59d6941cb2c9e2bb2da468f96f46fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b602a7f4ce62fae2b0346ec52b92af8f",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "0f4a81ce694589b3c9bc",
+    "url": "/dashboard/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "470cea0c33d2398ba9eb",
+    "url": "/dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f4a81ce694589b3c9bc",
+    "url": "/dashboard/static/js/2.5879a36b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboard/static/js/2.5879a36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470cea0c33d2398ba9eb",
+    "url": "/dashboard/static/js/main.e1cc67b2.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  }
+]);
